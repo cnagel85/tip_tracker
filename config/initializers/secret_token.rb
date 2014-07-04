67fd0b0b312de00a4d23844b24886cadf0b0a50f
Corey Nagel <coreyelliotnagel@gmail.com>
2014-07-04 12:32:15 -0600

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TipTracker::Application.config.secret_key_base = '1a7e5aa1c7e8cd7abdea65793e53c7aef7483679e77dd5760bf6bda87d7682ab5a00c9632f37a201e26ec131f91754e37a466a8887db1229070eb85b5667136e'
