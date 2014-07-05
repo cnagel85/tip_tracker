25194cc3d307ee6570b00f44958c57115d33c88a
Corey Nagel <coreyelliotnagel@gmail.com>
2014-07-04 20:44:51 -0600

    Finish static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,11 +1,30 @@
 require 'spec_helper'
 
-describe "StaticPages" do
-  describe "GET /static_pages" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get static_pages_index_path
-      response.status.should be(200)
+describe "Static pages" do
+
+  describe "Home page" do
+
+    it "should have the content 'Tip Tracker'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('Tip Tracker')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Tip Tracker | Home")
+    end
+  end
+
+  describe "Help page" do
+
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('Help')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/help'
+      expect(page).to have_title("Tip Tracker | Help")
     end
   end
 end
